fdd7eb824dffc90fec46c20b4f5945294620523b
ILPERSONA <kettstalin@gmail.com>
2026-05-06 19:54:12 +0300

    Ало

---
+++ alabuga.py
@@ -0,0 +1,22 @@
+# 1 Папка
+# Zadanie 1:
+# a = 10
+# b = 5
+# c = a + b
+# print(c)
+
+# radius = 7
+# area = S = 3.14*radius**2
+# print(area)
+
+# money = 2
+# Orel = 78
+
+# Grok = money * 50 * Orel
+# print(Grok)
+
+
+year = 2026
+age = 16
+ptover =  year - age
+print(ptover)
