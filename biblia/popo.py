72874228cda527f88c19e02bc5c9760fe702187a
ILPERSONA <kettstalin@gmail.com>
2026-05-14 19:24:34 +0300

    Хватит гореть

---
+++ biblia/popo.py
@@ -0,0 +1,5 @@
+MENTOR_NAME = 'ILPERSONA'
+MENTOR_LANGUAGE = 'Python'
+
+def print_mentor_info():
+    print(f'Ментор  {MENTOR_NAME} обучение языку  {MENTOR_LANGUAGE}')
---
+++ biblia/poppo2.py
@@ -0,0 +1,11 @@
+import popo
+
+from free_course.main4 import  calculate_total
+
+calculate_total('8')
+
+MENTOR_NAME= 'ILPERSONA'
+
+
+popo.print_mentor_info()
+print(MENTOR_NAME)
